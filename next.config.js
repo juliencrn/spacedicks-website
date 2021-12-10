7966adb2c7d8a1cb54814a712edb211b12b27e71
Julien <juliencaron@protonmail.com>
2021-12-10 19:22:20 +0100

    fix deploy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  webpack: (config, { webpack }) => {
+    config.plugins.push(new webpack.IgnorePlugin({
+      resourceRegExp: /^electron$/
+    }))
+    return config
+  },
   reactStrictMode: true,
   images: {
     domains: ['localhost', 'cryptodicks-api.herokuapp.com']
