fe184f40a2902c495babff05cf040416c92139ce
Julien <juliencaron@protonmail.com>
2021-12-11 17:18:20 +0100

    Fix svg error by webpack loader

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   webpack: (config, { webpack }) => {
+
+    // Fix "electron" error
     config.plugins.push(new webpack.IgnorePlugin({
       resourceRegExp: /^electron$/
     }))
+
+    // Set loader for svg images
+    const fileLoaderRule = config.module.rules.find(rule => rule.test && rule.test.test('.svg'))
+    fileLoaderRule.exclude = /\.svg$/
+    config.module.rules.push({
+      test: /\.svg$/,
+      loader: require.resolve('@svgr/webpack')
+    })
+
     return config
   },
   reactStrictMode: true,
