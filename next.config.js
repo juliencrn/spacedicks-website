fb4850142dde01b3ea3bb3d8357a6b9ec963cb89
Julien <juliencaron@protonmail.com>
2021-12-14 01:04:19 +0100

    chore: Update frontend url

--- next.config.js
+++ next.config.js
@@ -11,6 +11,6 @@ module.exports = {
   },
   reactStrictMode: true,
   images: {
-    domains: ['localhost', 'spacedicks.herokuapp.com', 'spacedicks.com']
+    domains: ['localhost', 'spacedicks.herokuapp.com', 'spacedicks.xyz']
   }
 }
