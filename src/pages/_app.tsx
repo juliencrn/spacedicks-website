c900aa8b09db12ff000a1f7e7d97c760dc938463
Julien <juliencaron@protonmail.com>
2021-12-17 04:51:54 +0100

    fix: seo and google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -18,8 +18,41 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <SEO />
-        <FaviconMeta />
+        <title>{title || "SpaceDicks"}</title>
+        <meta name="description" content={description} />
+
+        {/* <!-- Twitter Card data --> */}
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:title" content={title}/>
+        <meta name="twitter:description" content={description}/>
+        <meta name="twitter:creator" content={`@${twitterName}`}/>
+        <meta name="twitter:image:src" content={image.src}/>
+
+        {/* <!-- Open Graph data --> */}
+        <meta property="og:title" content={title} />
+        <meta property="og:type" content="site" />
+        <meta property="og:url" content="https://spacedicks.xyz" />
+        <meta property="og:image" content={image.src} />
+        <meta property="og:description" content={description} />
+
+        {/* <!-- Favicon --> */}
+        <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png"/>
+        <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png"/>
+        <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png"/>
+        <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png"/>
+        <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png"/>
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png"/>
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png"/>
+        <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
+        <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+        <link rel="manifest" href="/manifest.json"/>
+        <meta name="msapplication-TileColor" content="#ffffff"/>
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
+        <meta name="theme-color" content="#ffffff"/>
       </Head>
 
       <Web3ReactProvider getLibrary={getLibrary}>
@@ -34,8 +67,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-
-          gtag('config', 'G-B6SYN1Z959');
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
         `}
       </Script>
     </>
@@ -43,46 +75,3 @@ function MyApp({ Component, pageProps }: AppProps) {
 }
 
 export default MyApp
-
-const SEO = () => (
-  <>
-    <title>{title}</title>
-    <meta name="description" content={description} />
-
-    {/* <!-- Twitter Card data --> */}
-    <meta name="twitter:card" content="summary_large_image"/>
-    <meta name="twitter:title" content={title}/>
-    <meta name="twitter:description" content={description}/>
-    <meta name="twitter:creator" content={`@${twitterName}`}/>
-    <meta name="twitter:image:src" content={image.src}/>
-
-    {/* <!-- Open Graph data --> */}
-    <meta property="og:title" content={title} />
-    <meta property="og:type" content="site" />
-    <meta property="og:url" content="https://spacedicks.xyz" />
-    <meta property="og:image" content={image.src} />
-    <meta property="og:description" content={description} />
-  </>
-)
-
-const FaviconMeta = () => (
-  <>
-    <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png"/>
-    <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png"/>
-    <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png"/>
-    <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png"/>
-    <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png"/>
-    <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png"/>
-    <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png"/>
-    <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png"/>
-    <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png"/>
-    <link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png"/>
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png"/>
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-    <link rel="manifest" href="/manifest.json"/>
-    <meta name="msapplication-TileColor" content="#ffffff"/>
-    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png"/>
-    <meta name="theme-color" content="#ffffff"/>
-  </>
-)
